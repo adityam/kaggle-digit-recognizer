c181bbefb71d20b924e57d34cb20b56a7fe57968
Aditya Mahajan <adityam@umich.edu>
2013-03-09 18:17:03 -0500

    Refactoring

--- knn.hs
+++ knn.hs
@@ -1,5 +1,4 @@
 {-# Language OverloadedStrings, BangPatterns, FlexibleContexts #-}
-{-# Language GeneralizedNewtypeDeriving #-}
 
 -- K-nearest neighbors based solution for
 -- http://www.kaggle.com/c/digit-recognizer
@@ -28,6 +27,7 @@ import Data.Maybe (fromJust)
 import Control.Arrow ( (&&&) )
 
 import Control.Parallel.Strategies
+import Control.DeepSeq (NFData(..))
 
 -- Data structures for storing data
 import Data.Word (Word8)
@@ -38,37 +38,67 @@ import qualified Data.Vector.Storable as Vec (sum, zipWith, fromList, toList)
 record :: Parser [Word8]
 record = decimal `sepBy1` char ',' <?> "record"
 
-newtype Label         =  Label (Maybe Word8)
-            deriving (Eq, Ord, NFData)
-
-newtype FeatureVector =  FeatureVector (Vector Word8)
-
-data Record = Record !Label !FeatureVector
-
-mkLabeledRecord :: [Word8] -> Record
-mkLabeledRecord (x:xs) = Record (Label (Just x)) (FeatureVector (Vec.fromList xs))
-
-mkUnlabeledRecord :: [Word8] -> Record
-mkUnlabeledRecord xs   = Record (Label Nothing)  (FeatureVector (Vec.fromList xs))
+data Label = One
+           | Two
+           | Three
+           | Four
+           | Five
+           | Six
+           | Seven
+           | Eight
+           | Nine
+           | UnLabeled
+           deriving (Eq, Ord)
 
 instance Show Label where
-   show (Label l) = case l of 
-        Just d  -> show d
-        Nothing -> "-"
+      show One       = "1"
+      show Two       = "2"
+      show Three     = "3"
+      show Four      = "4"
+      show Five      = "5"
+      show Six       = "6"
+      show Seven     = "7"
+      show Eight     = "8"
+      show Nine      = "9"
+      show UnLabeled = "-"
 
-instance Show FeatureVector where
-    show (FeatureVector vec) = flatten . normalize . grid $ Vec.toList vec
-        where grid [] = []
-              grid y = let (a,as) = splitAt 28 y in a:grid as
-                        
-              normalize = map (map (\a -> if a > 128 then 1 else 0))
+instance NFData Label where
+      rnf a = a `seq` ()
 
-              flatten :: [[Word8]] -> String
-              flatten = concatMap (\a -> concatMap show a ++ "\n") 
+toLabel :: Word8 -> Label
+toLabel = go where
+      go 1 = One      
+      go 2 = Two      
+      go 3 = Three    
+      go 4 = Four     
+      go 5 = Five     
+      go 6 = Six      
+      go 7 = Seven    
+      go 8 = Eight    
+      go 9 = Nine     
+      go _ = UnLabeled
+  
+data Record = Record !Label (Vector Word8)
+
+withLabels :: [Word8] -> Record
+withLabels (x:xs) = Record (toLabel x) (Vec.fromList xs)
+
+withoutLabels :: [Word8] -> Record
+withoutLabels xs   = Record UnLabeled   (Vec.fromList xs)
+
+showVector :: Vector Word8 -> String
+showVector vec = flatten . normalize . grid $ Vec.toList vec
+    where grid [] = []
+          grid y = let (a,as) = splitAt 28 y in a:grid as
+                    
+          normalize = map (map (\a -> if a > 128 then 1 else 0))
+
+          flatten :: [[Word8]] -> String
+          flatten = concatMap (\a -> concatMap show a ++ "\n") 
 
 instance Show Record where
     show (Record l fv) = 
-        printf "Label: %s\n%s\n" (show l) (show fv)
+        printf "Label: %s\n%s\n" (show l) (showVector fv)
 
 label :: Record -> Label
 label (Record l _) = l 
@@ -80,19 +110,24 @@ label (Record l _) = l
 -- by first factorizing Q as A^T A. Then we can simply do a coordinate transform
 -- x -> Ax and do the usual Eucledian distance calculations.
 distance :: Record -> Record -> Double
-distance (Record _ (FeatureVector xs)) (Record _ (FeatureVector ys)) = Vec.sum diff 
+distance (Record _ xs) (Record _ ys) = Vec.sum diff 
     where diff = Vec.zipWith (\x y -> let d = fromIntegral (x-y) in d*d) xs ys
 
 {-# INLINE distance #-}
 
 classify :: [Record] -> Record -> Label
-classify !model !point = (fst majority)
+classify !model !point = fst majority
     where 
           majority = maximumBy (comparing snd) histogram
 
-          histogram :: [(Label, Int)]
-          histogram = [ (fst (head xs), length xs) 
-                      | xs <- groupBy ( (==) `on` fst) (sortBy (comparing fst) neighbors) ]
+          histogram :: [(Label, Double)]
+          histogram =  map (fst . head &&& weight) 
+                    .  groupBy ( (==) `on` fst )
+                    .  sortBy  (comparing fst )
+                    $  neighbors
+
+          weight :: [(Label, Double)] -> Double
+          weight = fromIntegral . length
 
           -- The naive implementation is slow, possibly because dist is being
           -- called O(n*lon(n)) times rather than O(n) times.
@@ -102,36 +137,35 @@ classify !model !point = (fst majority)
           dist      =  map (label &&& distance point) model `using` parListChunk 1000 rdeepseq
 
           neighbors :: [(Label, Double)]
-          neighbors = take k ( (sortBy (comparing snd) dist) `using` parListChunk 1000 rdeepseq)
+          neighbors = take k ((sortBy (comparing snd) dist) `using` parListChunk 1000 rdeepseq)
           
           -- Number of nearest neighbors
           k = 10 :: Int
 
-main :: IO ()
-main = let readLine :: ByteString -> [Word8]
+vectorize :: [ByteString] -> [[Word8]]
+vectorize = map readLine
+    where  readLine :: ByteString -> [Word8]
            readLine = fromJust . maybeResult . parse record
 
-           parseFile :: String -> IO [ByteString]
-           parseFile = fmap (tail . lines) . readFile
-       in
-       do
-       -- Load training sequences
-       trainingInput <- parseFile "data/train.csv"
-       let trainingData = map (mkLabeledRecord . readLine) trainingInput
+parseFile :: ([Word8] -> Record) -> FilePath -> IO [Record]
+parseFile lab fname = return . map lab . vectorize . tail . lines =<< readFile fname
 
-       -- Load testing sequences
-       testingInput <- parseFile "data/sample.csv"
-       let testingData = map (mkUnlabeledRecord . readLine) testingInput
+
+main :: IO ()
+main = do
+       -- Load sequences
+       trainingData <- parseFile withLabels    "data/train.csv" 
+       testingData  <- parseFile withoutLabels "data/sample.csv"
 
        -- Classify
        let classifier = classify trainingData
-           labels      = map classifier testingData 
+           labels     = map classifier testingData 
 
        -- Output result
        printf "Trainied using %d samples\n" (length trainingData)
        out <- openFile "data/output.csv" WriteMode     
 
-       -- Flush when the output has 100 bytes
+       -- Flush when the output has 200 bytes
        hSetBuffering out (BlockBuffering (Just 200))
 
        sequence_ [ hPrint out l | l <- labels ]
